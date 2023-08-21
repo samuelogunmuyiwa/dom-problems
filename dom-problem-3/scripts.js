f5bcb8f5702f7c04680849f723cde9a4bfaeaa7d
Larry-cool <larrycoolboy92@gmail.com>
2023-08-21 17:01:20 +0200

    function displaying the item in the droplist.

--- dom-problem-3/scripts.js
+++ dom-problem-3/scripts.js
@@ -10,3 +10,14 @@ function myFunction() {
 	var x = document.getElementById("mySelect").options.length;
 	p1.innerHTML = "There are " + x + " Cars in the list.";
 }
+
+
+let p2 = document.getElementById("demo");
+
+//define a function that displays the item in the list when you select from the dropdown.
+function myCarList() {
+
+	var y = document.getElementById("mySel");
+	var i = y.selectedIndex;
+	p2.innerHTML = y.options[i].text;
+}
