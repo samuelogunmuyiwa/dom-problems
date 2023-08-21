87f7f077ad24432c730f21a0607dcad2e87033a9
Larry-cool <larrycoolboy92@gmail.com>
2023-08-21 14:43:29 +0200

    assigned x variable to my function

--- dom-problem2/scripts.js
+++ dom-problem2/scripts.js
@@ -4,11 +4,11 @@ Then, write a JavaScript function to get the HREF attribute of the link.
 
 */
 
-const p1 = document.getElementById('p1');
+let p1 = document.getElementById('p1');
 
-let pLink = document.getElementById('link');
+//let pLink = document.getElementById('link');
 
-function getLink(){
-    const linkUrl = pLink.getAttribute('href');
-     p1.innerHTML = linkUrl;
-} 
+function myFunction() {
+  var x = document.getElementById("myAnchor").href;
+  p1.innerHTML = x;
+}
